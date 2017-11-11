49496ea7a9e21a76891cef36b64e2307c8dfee8d
cfaure004 <faure.clement5@gmail.com>
2017-11-11 13:51:23 +0100

    Delete news files and implement add and top files

--- js/top.js
+++ js/top.js
@@ -1,3 +1,44 @@
 $(function () {
     $("#header").load("header.html");
 })
+
+function upVote(ID, currentElement) {
+    var theColorIs = $(currentElement).css("color");
+    if(theColorIs=="rgb(51, 51, 51)") {
+        $(currentElement).css('color', 'green');
+
+        var number = parseInt($(ID).text());
+        number++;
+        $(ID).html(number.toString());
+
+    } else if (theColorIs == "rgb(0, 128, 0)") {
+        $(currentElement).css('color', "rgb(51, 51, 51)");
+
+        var number = parseInt($(ID).text());
+        number--;
+        $(ID).html(number.toString());
+    } else {
+        console.log("ERROR: This statement should not be called.");
+    }
+}
+
+function downVote(ID, currentElement) {
+    var theColorIs = $(currentElement).css("color");
+    if(theColorIs=="rgb(51, 51, 51)") {
+        $(currentElement).css('color', 'red');
+
+        var number = parseInt($(ID).text());
+        number--;
+        $(ID).html(number.toString());
+    } else if (theColorIs == "rgb(255, 0, 0)") {
+        $(currentElement).css('color', "rgb(51, 51, 51)");
+
+        var number = parseInt($(ID).text());
+        number++;
+        $(ID).html(number.toString());
+    } else {
+        console.log("ERROR: This statement should not be called.");
+    }
+
+}
+
